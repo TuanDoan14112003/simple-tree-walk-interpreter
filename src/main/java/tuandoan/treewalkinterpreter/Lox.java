833d239d302f09cdfab80b9183d88c2f52d7ec95
TuanDoan14112003 <tuandoan14112003@gmail.com>
2024-06-04 17:33:11 +1000

    feat: set up project

---
+++ src/main/java/tuandoan/treewalkinterpreter/Lox.java
@@ -0,0 +1,24 @@
+package tuandoan.treewalkinterpreter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            // runFile(args[0]);
+        } else {
+            // runPrompt();
+        }
+    }
+}
