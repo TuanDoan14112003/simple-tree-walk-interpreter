f9edcbfb54b5a7dd102bed19a961535959ac0235
Tuan Doan <tuandoan14112003@gmail.com>
2024-06-16 22:37:37 +1000

    feat: comma operator

--- src/main/java/tuandoan/treewalkinterpreter/lox/Parser.java
+++ src/main/java/tuandoan/treewalkinterpreter/lox/Parser.java
@@ -23,7 +23,19 @@ class Parser {
     }
 
     private Expr expression() {
-        return equality();
+        return comma();
+    }
+
+    private Expr comma() {
+        Expr expr = equality();
+
+        while (match(TokenType.COMMA)) {
+            Token operator = previous();
+            Expr right = equality();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
     }
 
     private Expr equality() {
