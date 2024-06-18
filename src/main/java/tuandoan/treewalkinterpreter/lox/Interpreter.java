48d4e4d35c6e4ec7f4bd4465eac11c4775e43c2c
TuanDoan14112003 <tuandoan14112003@gmail.com>
2024-06-18 16:30:48 +1000

    feat: add interpreter to evaluate expressions

---
+++ src/main/java/tuandoan/treewalkinterpreter/lox/Interpreter.java
@@ -0,0 +1,78 @@
+package tuandoan.treewalkinterpreter.lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+        return switch (expr.operator.type) {
+            case MINUS -> -(double) right;
+            case BANG -> !isTruthy(right);
+            default -> null;
+        };
+
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+                break;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+            case GREATER:
+                return (double)left > (double)right;
+            case GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case LESS:
+                return (double)left < (double)right;
+            case LESS_EQUAL:
+                return (double)left <= (double)right;
+            case BANG_EQUAL:
+                    return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+        }
+
+        return null;
+    }
+
+    private boolean isEqual(Object left, Object right) {
+        if (left == null & right == null) return true;
+        if(left == null) return false;
+
+        return left.equals(right);
+    }
+
+    private boolean isTruthy(Object expr) {
+        if (expr == null) return false;
+        if (expr instanceof Boolean) return (boolean)expr;
+        return true;
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+}
