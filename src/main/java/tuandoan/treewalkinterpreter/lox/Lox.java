c7f505839132c5db9d9e24adb27c1a43d30b3173
TuanDoan14112003 <tuandoan14112003@gmail.com>
2024-06-21 14:48:27 +1000

    feat: parse statements

--- src/main/java/tuandoan/treewalkinterpreter/lox/Lox.java
+++ src/main/java/tuandoan/treewalkinterpreter/lox/Lox.java
@@ -49,10 +49,9 @@ public class Lox {
         List<Token> tokens = scanner.scanTokens();
 
         Parser parser = new Parser(tokens);
-        Expr expression = parser.parse();
-
+        List<Stmt> statements = parser.parse();
         if (hadError) return;
-        interpreter.interpret(expression);
+        interpreter.interpret(statements);
     }
 
     static void error(int line, String message) {
