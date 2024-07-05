c3cf96cdd868824a622f7bf4f1abf1a308e64220
TuanDoan14112003 <tuandoan14112003@gmail.com>
2024-07-05 14:47:45 +1000

    feat: add function declaration syntax to the grammar

---
+++ src/main/java/tuandoan/treewalkinterpreter/lox/LoxFunction.java
@@ -0,0 +1,22 @@
+package tuandoan.treewalkinterpreter.lox;
+
+import java.util.List;
+
+class LoxFunction implements LoxCallable {
+    private final Stmt.Function declaration;
+
+    LoxFunction (Stmt.Function declaration) {
+        this.declaration = declaration;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        Environment environment = new Environment(interpreter.globals);
+        for (int i = 0; i < arguments.size(); ++i) {
+            environment.define(declaration.params.get(i).lexeme, arguments.get(i));
+        }
+
+        interpreter.executeBlock(declaration.body, environment);
+        return null;
+    }
+}
--- src/main/java/tuandoan/treewalkinterpreter/lox/Parser.java
+++ src/main/java/tuandoan/treewalkinterpreter/lox/Parser.java
@@ -27,6 +27,7 @@ class Parser {
 
     private Stmt declaration() {
         try {
+            if (match(TokenType.FUN)) return function("function");
             if (match(TokenType.VAR)) return varDeclaration();
             return statement();
         } catch (ParseError error) {
@@ -129,6 +130,26 @@ class Parser {
         Expr expr = expression();
         consume(TokenType.SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    private Stmt.Function function(String kind) {
+        Token name = consume(TokenType.IDENTIFIER, "Expect " + kind + " name.");
+        consume(TokenType.LEFT_PAREN, "Expect '(' after " + kind + " name.");
+        List<Token> parameters = new ArrayList<>();
+        if (!check(TokenType.RIGHT_PAREN)) {
+            do {
+                if (parameters.size() >= 255) {
+                    error(peek(), "Can't have more than 255 parameters");
+                }
+
+                parameters.add(consume(TokenType.IDENTIFIER, "Expect parameter name."));
+
+            } while(match(TokenType.COMMA));
+        }
+        consume(TokenType.RIGHT_PAREN, "Expect ')' after parameters.");
+        consume(TokenType.LEFT_PAREN, "Expect '{' before " + kind + " body");
+        List<Stmt> body = block();
+        return new Stmt.Function(name, parameters, body);
     }
 
     private List<Stmt> block() {
