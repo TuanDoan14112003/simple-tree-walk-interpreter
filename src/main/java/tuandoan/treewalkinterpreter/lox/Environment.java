d5f99d3ea44f0c114f77acde129a3df9897e9268
TuanDoan14112003 <tuandoan14112003@gmail.com>
2024-07-15 14:47:08 +1000

    feat: interpret resolved variables

--- src/main/java/tuandoan/treewalkinterpreter/lox/Environment.java
+++ src/main/java/tuandoan/treewalkinterpreter/lox/Environment.java
@@ -32,6 +32,19 @@ class Environment {
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+
+        return environment;
+    }
+
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
