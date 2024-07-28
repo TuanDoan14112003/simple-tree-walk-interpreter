c202e7f75f93ebc4e250ccbd3faf770cfe02543a
Tuan Doan <tuandoan14112003@gmail.com>
2024-07-28 17:45:14 +1000

    feat: add constructor

--- src/main/java/tuandoan/treewalkinterpreter/lox/LoxFunction.java
+++ src/main/java/tuandoan/treewalkinterpreter/lox/LoxFunction.java
@@ -5,9 +5,11 @@ import java.util.List;
 class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
     private final Environment closure;
+    private final boolean isInitializer;
 
 
-    LoxFunction (Stmt.Function declaration, Environment closure) {
+    LoxFunction (Stmt.Function declaration, Environment closure, boolean isInitializer) {
+        this.isInitializer = isInitializer;
         this.declaration = declaration;
         this.closure = closure;
     }
@@ -15,7 +17,7 @@ class LoxFunction implements LoxCallable {
     LoxFunction bind(LoxInstance instance) {
         Environment environment = new Environment(closure);
         environment.define("this", instance);
-        return new LoxFunction(declaration, environment);
+        return new LoxFunction(declaration, environment, isInitializer);
     }
 
     @Override
@@ -32,9 +34,11 @@ class LoxFunction implements LoxCallable {
         try {
             interpreter.executeBlock(declaration.body, environment);
         } catch (Return returnValue) {
+            if (isInitializer) return closure.getAt(0,"this");
             return returnValue.value;
         }
 
+        if (isInitializer) return closure.getAt(0, "this");
         return null;
     }
 
